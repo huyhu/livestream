74639b68753fc5b763738eb6d657ff0d6e69473b
huyhu <yigit.erkoca@gmail.com>
2017-12-27 02:14:50 +0100

    reverse last 5

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ var fs = require('fs');
 var path = require('path');
 var RaspiCam = require("raspicam");
 var rimraf = require('rimraf');
-var mkdirp = require('mkdirp');
 
 var spawn = require('child_process').spawn;
 var proc;
@@ -20,7 +19,43 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-var camera;
+rimraf('./timelapse');
+
+var camera = new RaspiCam({
+  mode: "timelapse",
+  output: "./timelapse/image_%02d.jpg", 
+  encoding: "jpg",
+  width: 640,
+  height: 480,
+  timelapse: 1000, // take a picture every 1 seconds
+  timeout: 999999999, // timeout of camera proc
+  q: 25
+});
+
+//listen for the "start" event triggered when the start method has been successfully initiated
+camera.on("start", function(){
+  console.log("camera started.");
+  streamStarted = true;
+});
+
+//listen for the "read" event triggered when each new photo/video is saved
+camera.on("read", function(err, timestamp, filename){ 
+  console.log("new image " + filename);
+  io.sockets.emit('liveStream', filename + '?_t=');
+});
+
+//listen for the "stop" event triggered when the stop method was called
+camera.on("stop", function(){
+  console.log("camera stopped");
+  streamStarted = false;
+});
+
+//listen for the process to exit when the timeout has been reached
+camera.on("exit", function(){
+  console.log("camera timeout.");
+  streamStarted = false;
+});
+
 var sockets = {};
 var people = {};
 var msgs = [];
@@ -88,41 +123,7 @@ function startStreaming(io) {
   if(streamStarted)
     return;
 
+  console.log('Start streaming..');
 
-  rimraf('./timelapse', function () {
-      camera = new RaspiCam({
-      mode: "timelapse",
-      output: "./timelapse/image_%02d.jpg", 
-      encoding: "jpg",
-      width: 640,
-      height: 480,
-      timelapse: 1000, // take a picture every 1 seconds
-      timeout: 999999999, // timeout of camera proc
-      q: 25
-    });
-
-      camera.on("start", function(){
-        console.log("camera started.");
-        streamStarted = true;
-      });
-
-      camera.on("read", function(err, timestamp, filename){ 
-        console.log("new image " + filename);
-        io.sockets.emit('liveStream', filename + '?_t=');
-      });
-
-      camera.on("stop", function(){
-        console.log("camera stopped");
-        streamStarted = false;
-      });
-
-      camera.on("exit", function(){
-        console.log("camera timeout.");
-        streamStarted = false;
-      });
-
-      console.log('Start streaming..');
-
-      camera.start();
-  });
+  camera.start();
 }
